38546fc83a1602075bd72fed59a0905ee4150448
Bartek7655 <bartekk194@gmail.com>
2025-10-26 14:34:39 +0100

    add first frame time

--- android/src/main/java/com/mrousavy/camera/react/RecordingStartEventModule.kt
+++ android/src/main/java/com/mrousavy/camera/react/RecordingStartEventModule.kt
@@ -1,23 +1,35 @@
-package com.mrousavy.camera.react
-
-import android.content.Context
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.Arguments
-import com.facebook.react.modules.core.DeviceEventManagerModule
-
 class RecordingStartEventModule(
   reactContext: ReactApplicationContext
 ) : ReactContextBaseJavaModule(reactContext) {
 
   override fun getName(): String = "RecordingStartEventModule"
 
+  @ReactMethod
+  fun addListener(eventName: String) {
+    // RN requirement, no-op
+  }
+
+  @ReactMethod
+  fun removeListeners(count: Int) {
+    // RN requirement, no-op
+  }
+
   companion object {
-    // Static helper, so we don't need to construct RecordingStartEventModule ourselves
     fun emitFirstFrameTimestamp(ctx: Context, firstFrameNs: Long) {
-      val reactCtx = ctx as? ReactApplicationContext
-        ?: (ctx as? com.facebook.react.uimanager.ThemedReactContext)?.reactApplicationContext
-        ?: return // can't emit, no react context
+      val reactCtx: ReactApplicationContext? =
+        when (ctx) {
+          is ReactApplicationContext -> ctx
+          is ThemedReactContext -> ctx.reactApplicationContext
+          else -> null
+        }
+
+      if (reactCtx == null) {
+        Log.w(
+          "RecordingStartEventModule",
+          "Cannot emit first frame timestamp: no ReactApplicationContext (ctx=${ctx.javaClass.name})"
+        )
+        return
+      }
 
       val payload = Arguments.createMap().apply {
         putDouble("firstFrameNs", firstFrameNs.toDouble())
